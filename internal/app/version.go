bc29975d59514b0bd64f5bac4c87f975971c670d
Go101 <22589241+go101@users.noreply.github.com>
2025-09-26 01:44:11 +0800

    v0.8.2

--- internal/app/version.go
+++ internal/app/version.go
@@ -12,9 +12,9 @@ import (
 	"go101.org/golds/internal/util"
 )
 
-const RoughBuildTime = "2025-08-30"
+const RoughBuildTime = "2025-09-26"
 
-const Version = "v0.8.1"
+const Version = "v0.8.2"
 
 func releaseGolds() {
 	if _, err := util.RunShell(time.Minute*3, "", nil, "go", "test", "./..."); err != nil {
